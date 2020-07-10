64d6b92879d33a506c73fd001a518839a95bb71b
Hada Ionut <hadaionut@gmail.com>
2020-07-10 22:45:50 +0300

    Aded a new really mean comment

--- src/module/reallyMeanComments.ts
+++ src/module/reallyMeanComments.ts
@@ -1,4 +1,5 @@
 export default [
     'Yet another failiure maybe you should reconsider your life',
-    'Hmm I think your players also rolled a nat 1 when you were born'
+    'Hmm I think your parents also rolled a nat 1 when you were born',
+    'Wow you are really trash you rolled [n] nat ones'
 ];
