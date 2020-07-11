701da45506237993a627685a39dc2e512eeac796
Hada Ionut <hadaionut@gmail.com>
2020-07-11 18:38:17 +0300

    Added more comments

--- src/module/meanComments.ts
+++ src/module/meanComments.ts
@@ -1,4 +1,8 @@
 export default [
     'Wow the algorithm is broken, you rolled above a 1. Let\'s fix that',
-    'YOU JUST LEARNED TO COUNT ABOVE A 1, you will forget that soon'
-];
+    'YOU JUST LEARNED TO COUNT ABOVE A 1, you will forget that soon',
+    'Who needs skill when you have luck...',
+    'Don\'t count on ever doing that again',
+    'Even a blind dwarf finds a gem once in a while',
+    
+]
--- src/module/reallyMeanComments.ts
+++ src/module/reallyMeanComments.ts
@@ -2,5 +2,8 @@ export default [
     'Yet another failure maybe you should reconsider your life',
     'Hmm I think your parents also rolled a nat 1 when you were born',
     'Wow you are really trash you rolled [sc-d1] nat ones',
-    'A powerful blow?!'
+    'A powerful blow?!',
+    'Look out below!',
+    'You meant to do that.',
+    'Your true skill shines through!',
 ];
