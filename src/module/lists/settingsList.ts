04cef883702bdc4df446d200b2567956e2033edc
Hada Ionut <hadaionut@gmail.com>
2020-07-21 17:05:25 +0300

    Created the strucutre for player costumisation
    Paramiters that will be costumizable:
    - critFail value
    - critSucc value
    - type of dice
    - the command header
    - the command word

--- src/module/lists/settingsList.ts
+++ src/module/lists/settingsList.ts
@@ -9,11 +9,56 @@ export default [
         },
     },
     {
-        key: "portraits",
+        key: "commandHeader",
         data: {
-            name: "Add portraits: ",
+            name: "Command header: ",
+            hint: "Type the character that should procede the command key word. Attention: (The \"/\" character does not work)",
             type: String,
-            default: "{}",
+            default: "!",
+            scope: "world",
+            config: true,
+        }
+    },
+    {
+        key: "commandKey",
+        data: {
+            name: "Command key word: ",
+            hint: "This is the word that when typed will trigger the display of the roll stats.",
+            type: String,
+            default: "sadness",
+            scope: "world",
+            config: true,
+        }
+    },
+    {
+        key: "diceType",
+        data: {
+            name: "Observed die",
+            hint: "This is the type of dice that the module will count and observe",
+            type: String,
+            default: "d20",
+            scope: "world",
+            config: true,
+        }
+    },
+    {
+        key: "critFail",
+        data: {
+            name: "Critical fail value",
+            hint: "This is the value that is considered a critical fail",
+            type: String,
+            default: "1",
+            scope: "world",
+            config: true,
+        }
+    },
+    {
+        key: "critSucces",
+        data: {
+            name: "Critical succes value",
+            hint: "This is the value that is considered a critical succes",
+            type: String,
+            default: "20",
             scope: "world",
             config: true,
         }
